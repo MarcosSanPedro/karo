fbce9a91e49d77ab966c50b4273ee447315a50aa
Marcos San Pedro <marcossanpedro68@gmail.com>
2025-01-10 21:08:36 -0500

    Update package.json dependencies and enhance accessibility in Footer.svelte; bump versions for several packages and add aria-label for TikTok link.

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,11 +9,11 @@
     <div class=" flex flex-col items-center gap-7">
     <h3 class="text-lg font-bold">Siguenos en nuestras redes sociales</h3>
 	<ul class=" flex gap-7">
-		<li class="duration-300 transform hover:scale-125"><a href="tiktok://user/@cassandra.zeledon">
+		<li class="duration-300 transform hover:scale-125"><a href="tiktok://user/@cassandra.zeledon" aria-label="TikTok">
 			<svg class=" w-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
 				><path
 					d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64h98.2V334.2H109.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H255V480H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"
-				/></svg
+				/></svg 
 			></a>
 		</li>
 		<li class="duration-300 transform hover:scale-125">
